b8a8c0e245c4783caf2e9c5700c6e0954b835043
dheeraj1566 <dheerajjangid013@gmail.com>
2025-05-08 16:58:43 +0530

    adjust cors error 2

--- back/index.js
+++ back/index.js
@@ -8,7 +8,6 @@ import inventoryRoutes from "./routes/inventoryRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import facultyAuth from "./routes/facultyAuth.js";
 import accountantRoutes from "./routes/accountantRoutes.js";
-// import storemanRoutes from "./routes/storemanRoute.js";
 import storemanRoutes from "./routes/storemanRoute.js";
 import cloudinary from "cloudinary";
 
